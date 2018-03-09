61111b76994651203a4943e39e1681a3a93b11c7
Javier Santacruz <javier.santacruz@avature.net>
2018-03-09 14:29:27 +0100

    Adds packaging resources

---
+++ setup.py
@@ -0,0 +1,21 @@
+import io
+import re
+from setuptools import setup
+
+
+def get_version_from_debian_changelog():
+    try:
+        with io.open('debian/changelog', encoding='utf8') as stream:
+            return re.search(r'\((.+)\)', next(stream)).group(1)
+    except Exception:
+        return '0.0.1'
+
+
+setup(
+    name='piglet',
+    version=get_version_from_debian_changelog(),
+    author='Platform',
+    author_email='platform@avature.net',
+    py_modules=['confight'],
+    install_requires=open('requirements.txt').read().splitlines(),
+)
