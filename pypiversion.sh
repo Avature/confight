84829e37755e7d14b7a24351db3c0a6ef488423d
Javier Santacruz <javier.santacruz@avature.net>
2018-06-20 18:13:08 +0200

    Adds pypi upload version script

---
+++ pypiversion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+echo "Cleaning dist directory"
+rm -f dist/*
+
+echo "Building python packages"
+python setup.py sdist
+python setup.py bdist_wheel
+python3 setup.py bdist_wheel
+echo "Generated python packages"
+
+echo "Uploading to test repository"
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
