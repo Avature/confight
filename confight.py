22bda77d06d5dcf7cc3137e76d8b318ffc14d423
Frank Lenormand <frank.lenormand@avature.net>
2024-06-10 17:58:36 +0200

    feat(confight): Add type annotations

--- confight.py
+++ confight.py
@@ -1,23 +1,33 @@
 from __future__ import print_function
 
-import io
-import os
-import sys
+import argparse
 import glob
+import io
+import itertools
 import json
 import logging
-import argparse
-import itertools
+import os
+import sys
 from collections import OrderedDict
 from configparser import ConfigParser, ExtendedInterpolation
+from logging import Logger
+from typing import IO, Any, Callable, Dict, List, Optional, Set
 
 import toml
 
-__version__ = '2.0.0-2'
-logger = logging.getLogger('confight')
+__version__: str = "2.0.0-2"
+logger: Logger = logging.getLogger("confight")
 
 
-def load_user_app(name, extension="toml", user_prefix=None, **kwargs):
+TConfigurationData = Dict[str, Any]
+TFormatLoader = Callable[[IO, str], TConfigurationData]
+TParser = Callable[[str, Optional[str]], TConfigurationData]
+TMerger = Callable[[List[TConfigurationData]], TConfigurationData]
+
+
+def load_user_app(
+    name: str, extension: str = "toml", user_prefix: Optional[str] = None, **kwargs
+) -> TConfigurationData:
     """Parse and merge app and user config from default locations
 
     User config will take precedence.
@@ -30,14 +40,16 @@ def load_user_app(name, extension="toml", user_prefix=None, **kwargs):
     :returns: Single dict with all the loaded config
     """
     if user_prefix is None:
-        user_prefix = os.path.join('~/.config', name)
-    filename = 'config.{ext}'.format(ext=extension)
-    kwargs.setdefault('user_file_path', os.path.join(user_prefix, filename))
-    kwargs.setdefault('user_dir_path', os.path.join(user_prefix, 'conf.d'))
+        user_prefix = os.path.join("~/.config", name)
+    filename = "config.{ext}".format(ext=extension)
+    kwargs.setdefault("user_file_path", os.path.join(user_prefix, filename))
+    kwargs.setdefault("user_dir_path", os.path.join(user_prefix, "conf.d"))
     return load_app(name, extension, **kwargs)
 
 
-def load_app(name, extension="toml", prefix=None, **kwargs):
+def load_app(
+    name: str, extension: str = "toml", prefix: Optional[str] = None, **kwargs
+) -> TConfigurationData:
     """Parse and merge app config from default locations
 
     :param name: Name of the application to load
@@ -48,15 +60,22 @@ def load_app(name, extension="toml", prefix=None, **kwargs):
     :returns: Single dict with all the loaded config
     """
     if prefix is None:
-        prefix = os.path.join('/etc', name)
-    filename = 'config.{ext}'.format(ext=extension)
-    kwargs.setdefault('file_path', os.path.join(prefix, filename))
-    kwargs.setdefault('dir_path', os.path.join(prefix, 'conf.d'))
+        prefix = os.path.join("/etc", name)
+    filename = "config.{ext}".format(ext=extension)
+    kwargs.setdefault("file_path", os.path.join(prefix, filename))
+    kwargs.setdefault("dir_path", os.path.join(prefix, "conf.d"))
     return load_app_paths(extension=extension, **kwargs)
 
 
-def load_app_paths(file_path=None, dir_path=None, user_file_path=None,
-                   user_dir_path=None, default=None, paths=None, **kwargs):
+def load_app_paths(
+    file_path: Optional[str] = None,
+    dir_path: Optional[str] = None,
+    user_file_path: Optional[str] = None,
+    user_dir_path: Optional[str] = None,
+    default: Optional[str] = None,
+    paths: Optional[str] = None,
+    **kwargs
+) -> TConfigurationData:
     """Parse and merge user and app config files
 
     User config will have precedence
@@ -65,33 +84,45 @@ def load_app_paths(file_path=None, dir_path=None, user_file_path=None,
     :param dir_path: Path to the extension config directory
     :param user_file_path: Path to the user base config file
     :param user_dir_path: Path to the user base config file
-    :param default: Path to be preppended as the default config file embedded
+    :param default: Path to be prepended as the default config file embedded
                     in the app
     :param paths: Extra paths to add to the parsing after the defaults
     :param force_extension: only read files with given extension.
     :returns: Single dict with all the loaded config
     """
     files = [default, file_path, dir_path, user_file_path, user_dir_path]
-    files += (paths or [])
+    files += paths or []
     return load_paths([path for path in files if path], **kwargs)
 
 
-def load_paths(paths, finder=None, extension=None,
-               force_extension=False, **kwargs):
+def load_paths(
+    paths: List[str],
+    finder: Optional[Callable[[str], List[str]]] = None,
+    extension: Optional[str] = None,
+    force_extension: bool = False,
+    **kwargs
+) -> TConfigurationData:
     """Parse and merge config in path and directories
 
+    :param paths: List of files to parse
     :param finder: Finder function(dir_path) returning ordered list of paths
+    :param extension: Extension of the files to filter
     :param force_extension: Only read files with given extension.
     :returns: Single dict with all the loaded config
     """
     finder = find if finder is None else finder
-    files = itertools.chain.from_iterable(finder(path) for path in paths)
+    files = list(itertools.chain.from_iterable(finder(path) for path in paths))
     if extension and force_extension:
-        files = (path for path in files if path.endswith('.' + extension))
+        files = [path for path in files if path.endswith("." + extension)]
     return load(files, **kwargs)
 
 
-def load(paths, format=None, parser=None, merger=None):
+def load(
+    paths: List[str],
+    format: Optional[str] = None,
+    parser: Optional[TParser] = None,
+    merger: Optional[TMerger] = None,
+) -> TConfigurationData:
     """Parse and merge a list of configuration files
 
     :param paths: List of files to parse
@@ -100,28 +131,31 @@ def load(paths, format=None, parser=None, merger=None):
     :param merger: Merge function(list_of_dicts) returning a dict
     :returns: Single dict with all the loaded config
     """
-    parser = parse if parser is None else parser
-    merger = merge if merger is None else merger
-    return merger([parser(path, format=format) for path in paths])
+    # NOTE: Mypy bug
+    # error: Incompatible types in assignment (expression has type "function", variable has type "Callable[[str, Optional[str]], Dict[str, Any]]")
+    # https://github.com/python/mypy/issues/16868
+    the_parser: TParser = parse if parser is None else parser  # type: ignore
+    the_merger: TMerger = merge if merger is None else merger
+    return the_merger([the_parser(path, format) for path in paths])
 
 
-def parse(path, format=None):
+def parse(path: str, format: Optional[str] = None) -> TConfigurationData:
     """Parse the config file at the given path
 
     :param path: Path to the config file
     :param format: Name of the format (default: guess from file extension)
     :returns: dict with the parsed contents
     """
-    format = format_from_path(path) if format is None else format
-    logger.info('Parsing %r config file from %r', format, path)
-    if format not in FORMATS:
-        raise ValueError('Unknown format {} for file {}'.format(format, path))
-    loader = FORMAT_LOADERS[format]
-    with io.open(path, 'r', encoding='utf8') as stream:
-        return loader(stream)
+    the_format: str = format_from_path(path) if format is None else format
+    logger.info("Parsing %r config file from %r", the_format, path)
+    if the_format not in FORMATS:
+        raise ValueError("Unknown format {} for file {}".format(the_format, path))
+    loader: TFormatLoader = FORMAT_LOADERS[the_format]
+    with io.open(path, "r", encoding="utf8") as stream:
+        return loader(stream, the_format)
 
 
-def merge(configs):
+def merge(configs: List[TConfigurationData]) -> TConfigurationData:
     """Merge list of dicts into a single dict
 
     For the same key, the last appearing value will prevail.
@@ -131,12 +165,10 @@ def merge(configs):
     :param configs: List of parsed config dicts in order
     :returns: dict with the merged resulting config
     """
-    logger.debug('Merging config data %r', configs)
+    logger.debug("Merging config data %r", configs)
     result = OrderedDict()
     # No OrderedSets available
-    keys = OrderedDict(
-        (key, None) for config in configs for key in config
-    )
+    keys = OrderedDict((key, None) for config in configs for key in config)
     for key in keys:
         values = [config[key] for config in configs if key in config]
         merges = [v for v in values if isinstance(v, dict)]
@@ -144,7 +176,7 @@ def merge(configs):
     return result
 
 
-def find(path):
+def find(path: str) -> List[str]:
     """Find files in the filesystem in order
 
     Expands and normalizes relative paths.
@@ -159,105 +191,102 @@ def find(path):
         return []
     if os.path.isfile(path):
         return [path]
-    return sorted(glob.glob(os.path.join(path, '*')))
+    return sorted(glob.glob(os.path.join(path, "*")))
 
 
-def check_access(path):
+def check_access(path: str) -> bool:
     """Return whether a config file or directory can be read"""
     if not path:
         return False
     elif not os.path.exists(path):
-        logger.debug('Could not find %r', path)
+        logger.debug("Could not find %r", path)
         return False
     elif not os.access(path, os.R_OK):
-        logger.error('Could not read %r', path)
+        logger.error("Could not read %r", path)
         return False
     elif os.path.isdir(path) and not os.access(path, os.X_OK):
-        logger.error('Could not list directory %r', path)
+        logger.error("Could not list directory %r", path)
         return False
     elif os.path.isfile(path) and os.access(path, os.X_OK):
-        logger.warning('Config file %r has exec permissions', path)
+        logger.warning("Config file %r has exec permissions", path)
     return True
 
 
-def load_ini(stream):
-    if 'ExtendedInterpolation' in globals():
+def load_json(stream: IO, format: Optional[str] = None) -> TConfigurationData:
+    return json.load(stream, object_pairs_hook=OrderedDict)
+
+
+def load_toml(stream: IO, format: Optional[str] = None) -> TConfigurationData:
+    return toml.load(stream, _dict=OrderedDict)
+
+
+def load_ini(stream: IO, format: Optional[str] = None) -> TConfigurationData:
+    if "ExtendedInterpolation" in globals():
         parser = ConfigParser(interpolation=ExtendedInterpolation())
     else:
         parser = ConfigParser()
     parser.read_file(stream)
-    return {
-        section: OrderedDict(parser.items(section))
-        for section in parser.sections()
-    }
+    return {section: OrderedDict(parser.items(section)) for section in parser.sections()}
 
 
-FORMATS = ('toml', 'ini', 'json')
-FORMAT_EXTENSIONS = {
-    'js': 'json',
-    'json': 'json',
-    'toml': 'toml',
-    'ini': 'ini',
-    'cfg': 'ini',
+FORMATS: Set[str] = {"toml", "ini", "json"}
+FORMAT_EXTENSIONS: Dict[str, str] = {
+    "js": "json",
+    "json": "json",
+    "toml": "toml",
+    "ini": "ini",
+    "cfg": "ini",
 }
-FORMAT_LOADERS = {
-    'json': lambda *args: json.load(*args, object_pairs_hook=OrderedDict),
-    'toml': lambda *args: toml.load(*args, _dict=OrderedDict),
-    'ini': load_ini
+FORMAT_LOADERS: Dict[str, TFormatLoader] = {
+    "json": load_json,
+    "toml": load_toml,
+    "ini": load_ini,
 }
 
 
 # Optional dependency yaml
 try:
-    from ruamel.yaml import YAML
+    from ruamel.yaml import YAML  # type: ignore
 except ImportError:
     pass
 else:
-    def load_yaml(stream):
+
+    def load_yaml(stream: IO, format: Optional[str] = None) -> TConfigurationData:
         yaml = YAML(typ="rt")
         return yaml.load(stream)
 
-    FORMATS = FORMATS + ('yaml',)
-    FORMAT_EXTENSIONS.update({
-        'yml': 'yaml',
-        'yaml': 'yaml'
-    })
-    FORMAT_LOADERS.update({
-        'yaml': load_yaml
-    })
+    FORMATS.add("yaml")
+    FORMAT_EXTENSIONS.update({"yml": "yaml", "yaml": "yaml"})
+    FORMAT_LOADERS.update({"yaml": load_yaml})
 
 # Optional dependency HCL
 try:
-    import hcl
+    import hcl  # type: ignore
 except ImportError:
     pass
 else:
-    def load_hcl(stream):
+
+    def load_hcl(stream: IO, format: Optional[str] = None) -> TConfigurationData:
         return hcl.load(stream)
 
-    FORMATS = FORMATS + ('hcl',)
-    FORMAT_EXTENSIONS.update({
-        'hcl': 'hcl'
-    })
-    FORMAT_LOADERS.update({
-        'hcl': load_hcl
-    })
+    FORMATS.add("hcl")
+    FORMAT_EXTENSIONS.update({"hcl": "hcl"})
+    FORMAT_LOADERS.update({"hcl": load_hcl})
 
 
-def format_from_path(path):
-    """Get file format from a given path based on exension"""
+def format_from_path(path: str) -> str:
+    """Get file format from a given path based on extension"""
     ext = os.path.splitext(path)[1][1:]  # extension without dot
     format = FORMAT_EXTENSIONS.get(ext)
     if not format:
-        raise ValueError(
-            'Unknown format extension {!r} for {!r}'.format(ext, path)
-        )
+        raise ValueError("Unknown format extension {!r} for {!r}".format(ext, path))
     return format
 
 
-def get_version():
+def get_version() -> str:
     import pkg_resources
-    return 'confight ' + pkg_resources.get_distribution('confight').version
+
+    return "confight " + pkg_resources.get_distribution("confight").version
 
 
 def cli_configure_logging(args):
@@ -267,40 +296,33 @@ def cli_configure_logging(args):
 
 def cli_show(args):
     """Load config and show it"""
-    config = load_user_app(
-        args.name, prefix=args.prefix, user_prefix=args.user_prefix
-    )
-    print(toml.dumps(config), end='')
+    config = load_user_app(args.name, prefix=args.prefix, user_prefix=args.user_prefix)
+    print(toml.dumps(config), end="")
 
 
 def cli():
-    LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
-    parser = argparse.ArgumentParser(
-        description='One simple way of parsing configs'
-    )
-    parser.add_argument('--version', action='version', version=get_version())
+    LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    parser = argparse.ArgumentParser(description="One simple way of parsing configs")
+    parser.add_argument("--version", action="version", version=get_version())
     parser.add_argument(
-        '-v', '--verbose', choices=LOG_LEVELS, default='ERROR',
-        help='Logging level default: ERROR'
+        "-v", "--verbose", choices=LOG_LEVELS, default="ERROR", help="Logging level default: ERROR"
     )
-    subparsers = parser.add_subparsers(title='subcommands', dest='command')
-    show_parser = subparsers.add_parser('show')
-    show_parser.add_argument('name', help='Name of the application')
-    show_parser.add_argument('--prefix', help='Base for default paths')
-    show_parser.add_argument(
-        '--user-prefix', help='Base for default user paths'
-    )
+    subparsers = parser.add_subparsers(title="subcommands", dest="command")
+    show_parser = subparsers.add_parser("show")
+    show_parser.add_argument("name", help="Name of the application")
+    show_parser.add_argument("--prefix", help="Base for default paths")
+    show_parser.add_argument("--user-prefix", help="Base for default user paths")
 
     args = parser.parse_args()
     cli_configure_logging(args)
     # Use callbacks, parser.set_defaults(func=) does not work in Python3.3
     callbacks = {
-        'show': cli_show,
+        "show": cli_show,
         None: lambda args: parser.print_help(file=sys.stderr),
     }
     try:
         callbacks[args.command](args)
     except Exception as error:
-        log = logger.exception if args.verbose == 'DEBUG' else logger.error
-        log('Error: %s', error)
+        log = logger.exception if args.verbose == "DEBUG" else logger.error
+        log("Error: %s", error)
         sys.exit(1)
